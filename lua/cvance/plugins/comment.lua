7f358e2662e5cf8ad099b225cbf051a347a51d97
PoundKake <vance.christian@gmail.com>
2024-09-06 09:53:02 -0400

    adding comment plugins

---
+++ lua/cvance/plugins/comment.lua
@@ -0,0 +1,31 @@
+return {
+  "numToStr/Comment.nvim",
+  event = { "BufReadPre", "BufNewFile" },
+  dependencies = {
+    "JoosepAlviste/nvim-ts-context-commentstring",
+  },
+  -- TODO: Normal Mode Usage
+  -- `gcc` - Toggles the current line using linewise comment
+  -- `gbc` - Toggles the current line using blockwise comment
+  -- `[count]gcc` - Toggles the number of line given as a prefix-count using linewise
+  -- `[count]gbc` - Toggles the number of line given as a prefix-count using blockwise
+  -- `gc[count]{motion}` - (Op-pending) Toggles the region using linewise comment
+  -- `gb[count]{motion}` - (Op-pending) Toggles the region using blockwise comment
+  --
+  -- BUG: Visual Mode usage
+  -- `gc` - Toggles the region using linewise comment
+  -- `gb` - Toggles the region using blockwise comment
+  --
+  config = function()
+    -- import comment plugin safely
+    local comment = require("Comment")
+
+    local ts_context_commentstring = require("ts_context_commentstring.integrations.comment_nvim")
+
+    -- enable comment
+    comment.setup({
+      -- for commenting tsx, jsx, svelte, html files
+      pre_hook = ts_context_commentstring.create_pre_hook(),
+    })
+  end,
+}
