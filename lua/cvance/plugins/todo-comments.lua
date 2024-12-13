a4cf4e03928bcecac9a6c50002e7c52856834f05
PoundKake <vance.christian@gmail.com>
2024-12-13 14:54:08 -0500

    formatting and added NOTE to list of highlights

--- lua/cvance/plugins/todo-comments.lua
+++ lua/cvance/plugins/todo-comments.lua
@@ -1,23 +1,24 @@
 return {
-  "folke/todo-comments.nvim",
-  event = { "BufReadPre", "BufNewFile" },
-  dependencies = { "nvim-lua/plenary.nvim" },
-  -- TODO: these will show up in telescope
-  -- HACK: navigate there tags with "[t" and "]t"
-  -- BUG: they also show colors
-  config = function()
-    local todo_comments = require("todo-comments")
-    -- set keymaps
-    local keymap = vim.keymap -- for conciseness
+	"folke/todo-comments.nvim",
+	event = { "BufReadPre", "BufNewFile" },
+	dependencies = { "nvim-lua/plenary.nvim" },
+	-- TODO: these will show up in telescope
+	-- HACK: navigate there tags with "[t" and "]t"
+	-- BUG: they also show colors
+	-- NOTE: like this green note here
+	config = function()
+		local todo_comments = require("todo-comments")
+		-- set keymaps
+		local keymap = vim.keymap -- for conciseness
 
-    keymap.set("n", "]t", function()
-      todo_comments.jump_next()
-    end, { desc = "Next todo comment" })
+		keymap.set("n", "]t", function()
+			todo_comments.jump_next()
+		end, { desc = "Next todo comment" })
 
-    keymap.set("n", "[t", function()
-      todo_comments.jump_prev()
-    end, { desc = "Previous todo comment" })
+		keymap.set("n", "[t", function()
+			todo_comments.jump_prev()
+		end, { desc = "Previous todo comment" })
 
-    todo_comments.setup()
-  end,
+		todo_comments.setup()
+	end,
 }
