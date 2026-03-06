c6e1ce984645e2a7ed081ddaffd5e97964dee627
PoundKake <vance.christian@gmail.com>
2026-03-06 12:38:57 -0500

    formatting and replacing old python formatters with ruff_format

--- lua/cvance/plugins/formatting.lua
+++ lua/cvance/plugins/formatting.lua
@@ -19,8 +19,8 @@ return {
 				graphql = { "prettier" },
 				liquid = { "prettier" },
 				lua = { "stylua" },
-        markdown = { "mdformat" },
-				python = { "isort", "black" },
+				markdown = { "mdformat" },
+				python = { "ruff_format" },
 			},
 			format_on_save = {
 				lsp_fallback = true,
